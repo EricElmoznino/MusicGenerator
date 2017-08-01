30174df26b818dc8e25bd21d74d5d20d5a8212b5
Eric <eric.elmoznino@gmail.com>
2017-08-01 00:04:09 -0400

    Making minor changes a bit everywhere

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ musician = MusicGenerator(hp.Configuration(epochs=200))
 # music = musician.generate(300, '/Users/Eric/ML_data/MusicGenerator/Every_Time_We_Touch_-_Chorus.mid',
 #                           './generated_music', 'test')
 musician.train('./training_songs')
-music = musician.generate(300, './training_songs/Every_Time_We_Touch_-_Chorus.mid',
+music = musician.generate(300, './training_songs/Rolling_In_The_Deep_-_Chorus.mid',
                           './generated_music', 'test')
