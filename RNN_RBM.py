059d131248d51ccc03511c8ac30b604496f4d7bd
Eric <eric.elmoznino@gmail.com>
2017-08-01 20:09:41 -0400

    RNN_RBM can now use either an lstm or a vanilla rnn

--- RNN_RBM.py
+++ RNN_RBM.py
@@ -4,22 +4,21 @@ import Helpers as hp
 
 class RNN_RBM:
 
-    def __init__(self, visible_size, hidden_size, state_size):
+    def __init__(self, visible_size, hidden_size, state_size, use_lstm=True):
         self.v_size = visible_size
         self.h_size = hidden_size
         self.s_size = state_size
+        self.use_lstm = use_lstm
 
         with tf.variable_scope('rbm'):
             self.W = hp.weight_variables([self.v_size, self.h_size], stddev=0.01)
 
         with tf.variable_scope('rnn'):
-            self.rnn = tf.contrib.rnn.BasicLSTMCell(self.s_size)
-            # self.rnn = tf.contrib.rnn.BasicRNNCell(self.s_size)
+            if use_lstm:
+                self.rnn = tf.contrib.rnn.BasicLSTMCell(self.s_size)
+            else:
+                self.rnn = tf.contrib.rnn.BasicRNNCell(self.s_size)
             self.rnn_s0 = self.rnn.zero_state(1, tf.float32)
-            # self.Wvu = hp.weight_variables([self.v_size, self.s_size], stddev=0.0001, name='weights_input')
-            # self.Wuu = hp.weight_variables([self.s_size, self.s_size], stddev=0.0001, name='weights_hidden')
-            # self.Bu = hp.bias_variables([self.s_size], value=0.0, name='biases_hidden')
-            # self.rnn_s0 = tf.Variable(tf.zeros([1, self.s_size]), name='initial_state')
 
         with tf.variable_scope('rnn_to_rbm'):
             self.Wuh = hp.weight_variables([self.s_size, self.h_size], stddev=0.0001, name='weights_h')
@@ -29,16 +28,14 @@ class RNN_RBM:
 
     def generation_model(self, x, length):
         with tf.variable_scope('generation'):
-            # primer_state = self.__unroll_rnn(x)[-1]
             _, primer_state = self.__unroll_rnn(x)
-            # primer_state = tf.reshape(primer_state, [1, -1])
 
         def music_timestep(t, k, x_t, s_tm1, music):
-            bh = tf.matmul(s_tm1.h, self.Wuh) + self.Buh
-            bv = tf.matmul(s_tm1.h, self.Wuv) + self.Buv
+            _s_tm1 = s_tm1.h if self.use_lstm else s_tm1
+            bh = tf.matmul(_s_tm1, self.Wuh) + self.Buh
+            bv = tf.matmul(_s_tm1, self.Wuv) + self.Buv
             rbm = RBM(self.W, bv, bh)
             notes_t = rbm.gibbs_sample(x_t, 25)
-            # s_t = tf.tanh(tf.matmul(notes_t, self.Wvu) + tf.matmul(s_tm1, self.Wuu) + self.Bu)
             _, s_t = self.rnn(notes_t, s_tm1)
             music = music + tf.concat([tf.zeros([t, self.v_size]), notes_t,
                                        tf.zeros([k-t-1, self.v_size])], 0)
@@ -46,7 +43,7 @@ class RNN_RBM:
 
         count = tf.constant(0)
         music = tf.zeros([length, self.v_size])
-        _, _, _, _, music = tf.while_loop(lambda  t, k, *args: t < k, music_timestep,
+        _, _, _, _, music = tf.while_loop(lambda t, k, *args: t < k, music_timestep,
                                           [count, length, tf.zeros([1, self.v_size]), primer_state, music],
                                           back_prop=False)
         return music
@@ -54,7 +51,8 @@ class RNN_RBM:
     def train_model(self, x):
         with tf.variable_scope('train_rnn_rbm'):
             states, _ = self.__unroll_rnn(x)
-            states_tm1 = tf.concat([self.rnn_s0.h, states], 0)[:-1, :]
+            state0 = self.rnn_s0.h if self.use_lstm else self.rnn_s0
+            states_tm1 = tf.concat([state0, states], 0)[:-1, :]
             bh = tf.matmul(states_tm1, self.Wuh) + self.Buh
             bv = tf.matmul(states_tm1, self.Wuv) + self.Buv
             rbm = RBM(self.W, bv, bh)
@@ -78,13 +76,6 @@ class RNN_RBM:
         return optimizer
 
     def __unroll_rnn(self, x):
-        # def step(s_tm1, x_t):
-        #     x_t = tf.reshape(x_t, [1, -1])
-        #     state_t = tf.tanh(tf.matmul(x_t, self.Wvu) + tf.matmul(s_tm1, self.Wuu) + self.Bu)
-        #     return state_t
-        # states = tf.scan(step, x, initializer=self.rnn_s0)
-        # states = tf.reshape(states, [-1, self.s_size])
-        # return states
         x = tf.reshape(x, [1, -1, self.v_size])
         states, final_state = tf.nn.dynamic_rnn(self.rnn, x, initial_state=self.rnn_s0)
         states = tf.reshape(states, [-1, self.s_size])
