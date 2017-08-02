5d6c1079d040ecbb1bccd5c27fc23b2918339b40
Eric <eric.elmoznino@gmail.com>
2017-08-01 20:10:10 -0400

    RNN_RBM can now use either an lstm or a vanilla rnn

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import Helpers as hp
 
 
 musician = MusicGenerator(hp.Configuration(epochs=500))
-musician.train('/Users/Eric/ML_data/MusicGenerator')
-music = musician.generate(300, '/Users/Eric/ML_data/MusicGenerator/Every_Time_We_Touch_-_Chorus.mid',
-                          './generated_music', 'test')
+# musician.train('/Users/Eric/ML_data/MusicGenerator')
+# music = musician.generate(300, '/Users/Eric/ML_data/MusicGenerator/Every_Time_We_Touch_-_Chorus.mid',
+#                           './generated_music', 'test')
 musician.train('./training_songs')
 music = musician.generate(300, './training_songs/Every_Time_We_Touch_-_Chorus.mid',
                           './generated_music', 'test')
