d8b6debf3e7aef4259ef0a82be43cdf52a984af6
Eric <eric.elmoznino@gmail.com>
2017-08-03 10:39:30 -0400

    Modifying parameters to try and get optimal music

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=500, train_log_path='./train5'))
+musician = MusicGenerator(hp.Configuration(epochs=500, train_log_path='./train'))
 # musician.train('/Users/Eric/ML_data/MusicGenerator')
 # music = musician.generate(300, '/Users/Eric/ML_data/MusicGenerator/Every_Time_We_Touch_-_Chorus.mid',
 #                           './generated_music', 'test')
 musician.train('./training_songs/Classical')
 music = musician.generate(300, './training_songs/Classical/043-BEETHOVEN_-_Op-031_No-02_Piano_Sonata_No-17_D-min_1st-Mov_Tempest_1802_(pb_Tuncali).mid',
-                          './generated_music', 'test5')
+                          './generated_music', 'test')
