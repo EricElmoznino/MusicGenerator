0fceb4bcfb0f7540569e306607c7619c30544a70
Eric <eric.elmoznino@gmail.com>
2017-08-05 19:18:35 -0400

    Training Jazz

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=500, train_log_path='./train1m50h100s4t'))
+musician = MusicGenerator(hp.Configuration(epochs=500, train_log_path='./train1m450h100s4tlstm'))
 musician.train('./training_songs/Jazz')
 music = musician.generate(400, './training_songs/Jazz/005-A_Nighting.mid',
-                          './generated_music', '1m50h100s4t')
+                          './generated_music', '4m50h100s4tlstm')
