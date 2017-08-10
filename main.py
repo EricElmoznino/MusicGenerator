9b2022e6d3a0b1ebdf3f13ee2d2cd449bdf35e14
Eric <eric.elmoznino@gmail.com>
2017-08-10 16:02:46 -0400

    Training on Nottingham dataset

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./jazz', num_timesteps=4))
+musician = MusicGenerator(hp.Configuration(epochs=500, train_log_path='./jazz', num_timesteps=4))
 musician.train('./training_songs/Nottingham')
 music = musician.generate(400, './training_songs/Nottingham/ashover_simple_chords_5.mid',
                           './generated_music', 'jazz')
