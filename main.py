0a6ca083a847f65e1fe5afa47e169ff489450584
Eric <eric.elmoznino@gmail.com>
2017-08-10 20:44:45 -0400

    Trying a different midi library and data format (piano roll)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./rnn'))
-musician.train('./training_songs/Nottingham')
-music = musician.generate(3200, './training_songs/Nottingham/ashover_simple_chords_16.mid',
-                          './generated_music', 'rnn')
+musician = MusicGenerator(hp.Configuration(epochs=400, train_log_path='./jazzrnn'))
+musician.train('./training_songs/Jazz')
+music = musician.generate(3200, './training_songs/Jazz/005-A_Nighting.mid',
+                          './generated_music', 'jazzrnn')
