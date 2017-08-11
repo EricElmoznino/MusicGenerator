ab07e90cb4185e91b6548eb4fd4d74603090c31d
Eric <eric.elmoznino@gmail.com>
2017-08-11 14:59:30 -0400

    Finding optimal parameters with new piano roll data format

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./jasslstm4m'))
+musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./jasslstm4m', num_timesteps=4))
 musician.train('./training_songs/Jazz')
 music = musician.generate(3200, './training_songs/Jazz/005-A_Nighting.mid',
                           './generated_music', 'jasslstm4m')
