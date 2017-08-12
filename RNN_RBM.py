816c9d5579dd23851cddf245648939caee0c1baf
Eric <eric.elmoznino@gmail.com>
2017-08-11 23:28:20 -0400

    Trying to use lstm memory state and hidden state as inputs to the rbm

--- RNN_RBM.py
+++ RNN_RBM.py
@@ -27,42 +27,60 @@ class RNN_RBM:
             self.rnn_s0 = self.rnn.zero_state(1, tf.float32)
 
         with tf.variable_scope('rnn_to_rbm'):
-            self.Wuh = hp.weight_variables([self.s_size, self.h_size], stddev=0.0001, name='weights_h')
-            self.Wuv = hp.weight_variables([self.s_size, self.v_size], stddev=0.0001, name='weights_v')
+            self.Wuh = hp.weight_variables([self.s_size, self.h_size], stddev=0.0001, name='weights_uh')
+            self.Wuv = hp.weight_variables([self.s_size, self.v_size], stddev=0.0001, name='weights_uv')
+            self.Wqh = hp.weight_variables([self.s_size, self.h_size], stddev=0.0001, name='weights_qh')
+            self.Wqv = hp.weight_variables([self.s_size, self.v_size], stddev=0.0001, name='weights_qv')
             self.Buh = hp.bias_variables([self.h_size], value=0.0, name='biases_h')
             self.Buv = hp.bias_variables([self.v_size], value=0.0, name='biases_v')
 
     def generation_model(self, x, length):
         with tf.variable_scope('generation'):
-            _, primer_state = self.__unroll_rnn(x)
+            # _, primer_state = self.__unroll_rnn(x)
+            primer_state = self.__unroll_rnn(x)
+            primer_u, primer_q = primer_state.h[-1], primer_state.c[-1]
 
-        def music_timestep(t, k, x_t, s_tm1, music):
-            _s_tm1 = s_tm1[-1] if self.num_rnn_cells > 1 else s_tm1
-            _s_tm1 = _s_tm1.h if self.use_lstm else _s_tm1
-            bh = tf.matmul(_s_tm1, self.Wuh) + self.Buh
-            bv = tf.matmul(_s_tm1, self.Wuv) + self.Buv
+        # def music_timestep(t, k, x_t, s_tm1, music):
+        def music_timestep(t, k, x_t, u_tm1, q_tm1, music):
+            # _s_tm1 = s_tm1[-1] if self.num_rnn_cells > 1 else s_tm1
+            # _s_tm1 = _s_tm1.h if self.use_lstm else _s_tm1
+            # bh = tf.matmul(_s_tm1, self.Wuh) + self.Buh
+            # bv = tf.matmul(_s_tm1, self.Wuv) + self.Buv
+            bh = tf.matmul(u_tm1, self.Wuh) + tf.matmul(q_tm1, self.Wqh) + self.Buh
+            bv = tf.matmul(u_tm1, self.Wuv) + tf.matmul(q_tm1, self.Wqv) + self.Buv
             rbm = RBM(self.W, bv, bh)
             notes_t = rbm.gibbs_sample(x_t, 25)
-            _, s_t = self.rnn(notes_t, s_tm1)
+            _, s_t = self.rnn(notes_t, tf.contrib.rnn.LSTMStateTuple(u_tm1, q_tm1))
             music = music + tf.concat([tf.zeros([t, self.v_size]), notes_t,
                                        tf.zeros([k-t-1, self.v_size])], 0)
-            return t+1, k, notes_t, s_t, music
+            # return t+1, k, notes_t, s_t, music
+            return t+1, k, notes_t, u_tm1, q_tm1, music
 
         count = tf.constant(0)
         music = tf.zeros([length, self.v_size])
-        _, _, _, _, music = tf.while_loop(lambda t, k, *args: t < k, music_timestep,
-                                          [count, length, tf.zeros([1, self.v_size]), primer_state, music],
+        # _, _, _, _, music = tf.while_loop(lambda t, k, *args: t < k, music_timestep,
+        #                                   [count, length, tf.zeros([1, self.v_size]), primer_state, music],
+        #                                   back_prop=False)
+        _, _, _, _, _, music = tf.while_loop(lambda t, k, *args: t < k, music_timestep,
+                                          [count, length, tf.zeros([1, self.v_size]), primer_u, primer_q, music],
                                           back_prop=False)
         return music
 
     def train_model(self, x):
         with tf.variable_scope('train_rnn_rbm'):
-            states, _ = self.__unroll_rnn(x)
-            state0 = self.rnn_s0[-1] if self.num_rnn_cells > 1 else self.rnn_s0
-            state0 = state0.h if self.use_lstm else state0
-            states_tm1 = tf.concat([state0, states], 0)[:-1, :]
-            bh = tf.matmul(states_tm1, self.Wuh) + self.Buh
-            bv = tf.matmul(states_tm1, self.Wuv) + self.Buv
+            # states, _ = self.__unroll_rnn(x)
+            # state0 = self.rnn_s0[-1] if self.num_rnn_cells > 1 else self.rnn_s0
+            # state0 = state0.h if self.use_lstm else state0
+            # states_tm1 = tf.concat([state0, states], 0)[:-1, :]
+            # bh = tf.matmul(states_tm1, self.Wuh) + self.Buh
+            # bv = tf.matmul(states_tm1, self.Wuv) + self.Buv
+            states = self.__unroll_rnn(x)
+            u_t = tf.reshape(states.h, [-1, self.s_size])
+            q_t = tf.reshape(states.c, [-1, self.s_size])
+            u_tm1 = tf.concat([self.rnn_s0.h, u_t], 0)[:-1, :]
+            q_tm1 = tf.concat([self.rnn_s0.h, q_t], 0)[:-1, :]
+            bh = tf.matmul(u_tm1, self.Wuh) + tf.matmul(q_tm1, self.Wqh) + self.Buh
+            bv = tf.matmul(u_tm1, self.Wuv) + tf.matmul(q_tm1, self.Wqv) + self.Buv
             rbm = RBM(self.W, bv, bh)
 
         with tf.variable_scope('train_ops'):
@@ -84,8 +102,14 @@ class RNN_RBM:
         return cost, optimizer
 
     def __unroll_rnn(self, x):
-        x = tf.reshape(x, [1, -1, self.v_size])
-        states, final_state = tf.nn.dynamic_rnn(self.rnn, x, initial_state=self.rnn_s0)
-        states = tf.reshape(states, [-1, self.s_size])
-        return states, final_state
-
+        # x = tf.reshape(x, [1, -1, self.v_size])
+        # states, final_state = tf.nn.dynamic_rnn(self.rnn, x, initial_state=self.rnn_s0)
+        # states, _ = tf.nn.dynamic_rnn(self.rnn, x, initial_state=self.rnn_s0)
+        # states = tf.reshape(states, [-1, self.s_size])
+        # return states, final_state
+        def recurrence(s_tm1, _x):
+            _x = tf.reshape(_x, [1, self.v_size])
+            _, s_t = self.rnn(_x, s_tm1)
+            return s_t
+        states = tf.scan(recurrence, x, initializer=self.rnn_s0)
+        return states
