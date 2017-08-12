a550c913350c26dc259f157e9afb3803191d90b9
Eric <eric.elmoznino@gmail.com>
2017-08-12 10:52:47 -0400

    Trying to use lstm memory state and hidden state as inputs to the rbm

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./jazz'))
+musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./jazz4m'))
 musician.train('./training_songs/Jazz')
 music = musician.generate(3200, './training_songs/Jazz/005-A_Nighting.mid',
-                          './generated_music', 'jazz')
+                          './generated_music', 'jazz4m')
