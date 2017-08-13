b5cf3750466fba1f5530f3eb14a0c8311cf91972
EricElmoznino <eric.elmoznino@gmail.com>
2017-08-13 15:18:56 -0400

    Training on pop music

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./pop'))
+musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./pop4m'))
 musician.train('./training_songs/Pop')
 music = musician.generate(3200, './training_songs/Pop/I_Kissed_A_Girl_-_Chorus.mid',
-                          './generated_music', 'pop')
+                          './generated_music', 'pop4m')
