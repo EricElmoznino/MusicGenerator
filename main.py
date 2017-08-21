93151677d020e0d409976bec67be7a4626d6a039
EricElmoznino <eric.elmoznino@gmail.com>
2017-08-21 11:24:46 -0400

    Debugging deep belief net

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./dbn', pretrain_epochs=0))
-musician.train('./training_songs/Nottingham')
+musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./lstmnew_no_theano_pop', pretrain_epochs=0))
+musician.train('./training_songs/Pop')
 music = musician.generate(3200, './training_songs/Pop/I_Kissed_A_Girl_-_Chorus.mid',
-                          './generated_music', 'dbn')
+                          './generated_music', 'lstmnew_no_theano_pop')
