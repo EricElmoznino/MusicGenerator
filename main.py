bd80ef97b252c143e5edb2b60c36aea90fdd5ba0
EricElmoznino <eric.elmoznino@gmail.com>
2017-08-21 16:13:43 -0400

    Debugging deep belief net

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./dbnjazz', pretrain_epochs=0))
-musician.train('./training_songs/Pop')
-music = musician.generate(3200, './training_songs/Pop/I_Kissed_A_Girl_-_Chorus.mid',
+# musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./pop', pretrain_epochs=0))
+# musician.train('./training_songs/Pop')
+# music = musician.generate(3200, './training_songs/Pop/I_Kissed_A_Girl_-_Chorus.mid',
+#                           './generated_music', 'pop')
+musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./dbnjazz', pretrain_epochs=0))
+musician.train('./training_songs/Jazz')
+music = musician.generate(3200, './training_songs/Jazz/005-A_Nighting.mid',
                           './generated_music', 'dbnjazz')
-# musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./jazz', pretrain_epochs=0))
-# musician.train('./training_songs/Jazz')
-# music = musician.generate(3200, './training_songs/Jazz/005-A_Nighting.mid',
-#                           './generated_music', 'jazz')
