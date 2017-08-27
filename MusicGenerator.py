f2871a09c7e1b5f3a0746fb6bad5715fe0894d1c
EricElmoznino <eric.elmoznino@gmail.com>
2017-08-27 13:10:51 -0400

    Finding optimal parameters to generate best music

--- MusicGenerator.py
+++ MusicGenerator.py
@@ -15,7 +15,7 @@ class MusicGenerator:
         self.conf = configuration
         self.manipulator = mm.MidiManipulator(self.conf.num_timesteps)
 
-        self.rnn_rbm = RNN_DBN(self.manipulator.input_length, [100], 100)
+        self.rnn_rbm = RNN_DBN(self.manipulator.input_length, [75], 100, num_rnn_cells=2)
 
         with tf.variable_scope('inputs'):
             self.x = tf.placeholder(tf.float32, shape=[None, self.manipulator.input_length])
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./pop100h2m'))
+musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./pop75h2m'))
 musician.train('./training_songs/Pop')
 music = musician.generate(3200, './training_songs/Pop/I_Kissed_A_Girl_-_Chorus.mid',
-                          './generated_music', 'pop100h2m')
+                          './generated_music', 'pop75h2m')
 # musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./jazz', pretrain_epochs=0))
 # musician.train('./training_songs/Jazz')
 # music = musician.generate(3200, './training_songs/Jazz/005-A_Nighting.mid',
