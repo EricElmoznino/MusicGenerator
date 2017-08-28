b6ca7b7adb442f490e99c4755426dda0b1658fef
EricElmoznino <eric.elmoznino@gmail.com>
2017-08-27 23:36:48 -0400

    Finding optimal parameters to generate best music

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./pop100h'))
-musician.train('./training_songs/Pop')
-music = musician.generate(3200, './training_songs/Pop/I_Kissed_A_Girl_-_Chorus.mid',
-                          './generated_music', 'pop100h')
-# musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./jazz75h'))
-# musician.train('./training_songs/Jazz')
-# music = musician.generate(3200, './training_songs/Jazz/005-A_Nighting.mid',
-#                           './generated_music', 'jazz75h')
+# musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./pop'))
+# musician.train('./training_songs/Pop')
+# music = musician.generate(3200, './training_songs/Pop/I_Kissed_A_Girl_-_Chorus.mid',
+#                           './generated_music', 'pop')
+musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./jazz75h'))
+musician.train('./training_songs/Jazz')
+music = musician.generate(3200, './training_songs/Jazz/005-A_Nighting.mid',
+                          './generated_music', 'jazz75h')
 # musician = MusicGenerator(hp.Configuration(epochs=300, train_log_path='./classical75h2m'))
 # musician.train('./training_songs/Classical')
 # music = musician.generate(3200, './training_songs/Classical/045-BEETHOVEN_-_Op-031_No-03_Piano_Sonata_No-18_E-flat-Maj_The_Hunt_(pb_Koleso).mid',
