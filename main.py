2864343e71c30d28575752964c01fa75abe0ad0b
EricElmoznino <eric.elmoznino@gmail.com>
2017-08-28 18:24:56 -0400

    Finding optimal parameters to generate best music

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import Helpers as hp
 # musician.train('./training_songs/Jazz')
 # music = musician.generate(1000, './training_songs/Jazz/005-A_Nighting.mid',
 #                           './generated_music', 'jazz')
-musician = MusicGenerator(hp.Configuration(epochs=100, train_log_path='./classical75h'))
+musician = MusicGenerator(hp.Configuration(epochs=100, train_log_path='./classical75h2m'))
 musician.train('./training_songs/Classical')
 music = musician.generate(1000, './training_songs/Classical/045-BEETHOVEN_-_Op-031_No-03_Piano_Sonata_No-18_E-flat-Maj_The_Hunt_(pb_Koleso).mid',
-                          './generated_music', 'classical75h')
+                          './generated_music', 'classical75h2m')
 # musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./nottingham', pretrain_epochs=0))
 # musician.train('./training_songs/Nottingham')
 # music = musician.generate(1000, './training_songs/Nottingham/ashover_simple_chords_5.mid',
