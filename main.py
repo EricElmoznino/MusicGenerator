7b250d404579b9416e7d77dfb0a978ed88e03fec
EricElmoznino <eric.elmoznino@gmail.com>
2017-08-28 23:15:09 -0400

    Finding optimal parameters to generate best music

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from MusicGenerator import MusicGenerator
 import Helpers as hp
 
 
-# musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./pop50h'))
+# musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./pop'))
 # musician.train('./training_songs/Pop')
 # music = musician.generate(1000, './training_songs/Pop/I_Kissed_A_Girl_-_Chorus.mid',
-#                           './generated_music', 'pop50h')
-musician = MusicGenerator(hp.Configuration(epochs=100, train_log_path='./jazz100h4m'))
-musician.train('./training_songs/Jazz')
-music = musician.generate(1000, './training_songs/Jazz/005-A_Nighting.mid',
-                          './generated_music', 'jazz100h4m')
-# musician = MusicGenerator(hp.Configuration(epochs=100, train_log_path='./classical100h'))
-# musician.train('./training_songs/Classical')
-# music = musician.generate(1000, './training_songs/Classical/045-BEETHOVEN_-_Op-031_No-03_Piano_Sonata_No-18_E-flat-Maj_The_Hunt_(pb_Koleso).mid',
-#                           './generated_music', 'classical100h')
+#                           './generated_music', 'pop')
+# musician = MusicGenerator(hp.Configuration(epochs=100, train_log_path='./jazz'))
+# musician.train('./training_songs/Jazz')
+# music = musician.generate(1000, './training_songs/Jazz/005-A_Nighting.mid',
+#                           './generated_music', 'jazz')
+musician = MusicGenerator(hp.Configuration(epochs=100, train_log_path='./classical100h4m'))
+musician.train('./training_songs/Classical')
+music = musician.generate(1000, './training_songs/Classical/045-BEETHOVEN_-_Op-031_No-03_Piano_Sonata_No-18_E-flat-Maj_The_Hunt_(pb_Koleso).mid',
+                          './generated_music', 'classical100h4m')
 # musician = MusicGenerator(hp.Configuration(epochs=200, train_log_path='./nottingham', pretrain_epochs=0))
 # musician.train('./training_songs/Nottingham')
 # music = musician.generate(1000, './training_songs/Nottingham/ashover_simple_chords_5.mid',
